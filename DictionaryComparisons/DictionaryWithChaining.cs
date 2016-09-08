4b46a028c0dc09b5d002263ead74d5addcc4c008
Luke Englund <Luke.Englund@Englund.combrew>
2016-09-07 19:47:50 -0700

    Starting fresh.

--- DictionaryComparisons/DictionaryWithChaining.cs
+++ DictionaryComparisons/DictionaryWithChaining.cs
@@ -10,33 +10,6 @@ namespace DictionaryComparisons.Implementation
 
     public class DictionaryWithChaining<TKey, TVal>
     {
-        // The initial size of the array 
-        const int INITIAL_SIZE = 16;
-        // The loading percentage before resizing
-        const float LOADING_LIMIT_PRCNT = 0.90f;
-
-        // Table that holds the lists of key/value pairs
-        private List<KeyValuePair<TKey, TVal>>[] _table;
-
-        private int _arraySize = -1;
-
-        public DictionaryWithChaining()
-        {
-            _arraySize = INITIAL_SIZE;
-            
-            // Check that key type is IEquatable and Hashable
-            Init();
-        }
-
-        private void Init()
-        {
-            table = new List<KeyValuePair<TKey, TVal>>[_arraySize];
-        }
-
-        // Takes a key and provides array position
-        private int GetArrayPosition(TKey key)
-        {
-            return 
-        }
+        
     }
 }
